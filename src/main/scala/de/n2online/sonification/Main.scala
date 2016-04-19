72f72063076f1519333001fe742bc92dd9b5bd70
n2code <n2code@users.noreply.github.com>
2016-04-19 12:31:37 +0200

    soundtesting

--- src/main/scala/de/n2online/sonification/Main.scala
+++ src/main/scala/de/n2online/sonification/Main.scala
@@ -19,8 +19,27 @@ import javafx.util.Duration
 
 import org.apache.commons.math3.util.FastMath
 
+import de.sciss.synth._
+import ugen._
+import Ops._
+
 object Sonification {
   def main(args: Array[String]) {
+    val cfg = Server.Config()
+    cfg.program = "/usr/bin/scsynth"
+    // runs a server and executes the function
+    // when the server is booted, with the
+    // server as its argument
+    Server.run(cfg) { s =>
+      // play is imported from package de.sciss.synth.
+      // it provides a convenience method for wrapping
+      // a synth graph function in an `Out` element
+      // and playing it back.
+      play {
+        val f = LFSaw.kr(0.4).madd(24, LFSaw.kr(Seq(8, 7.23)).madd(3, 80)).midicps
+        CombN.ar(SinOsc.ar(f) * 0.04, 0.2, 0.2, 4)
+      }
+    }
     Application.launch(classOf[Main], args: _*)
   }
 }
