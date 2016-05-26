abfe61e0d37008af13697910df0f97416b5de5f6
n2code <n2code@users.noreply.github.com>
2016-05-26 16:55:08 +0200

    optimized frequency test

--- src/main/scala/de/n2online/sonification/generators/BasicBeepFreqPanned.scala
+++ src/main/scala/de/n2online/sonification/generators/BasicBeepFreqPanned.scala
@@ -16,9 +16,9 @@ class BasicBeepFreqPanned() extends Generator {
 
   override def initialize(server: Server): Unit = {
     val notedef = Some(SynthDef("NoteGen") {
-      val vol = LinLin("linVol".kr(0.0), 0.0, 1.0, 0.1, 1.0)
+      val vol = LinExp.ar("linVol".kr(0.0), 0.0, 1.0, 0.2, 1.0)
 
-      val freq = LinExp.ar("freqLin".kr(0.0), 0.01, 1.0, 110.0, 220.0)
+      val freq = LinExp.ar("freqLin".kr(0.0), 0.01, 1.0, 55.0, 220.0)
       val pure = SinOsc.ar(freq / 2) * 0.1 + SinOsc.ar(freq) * 0.25 + Pulse.ar(freq) * 0.1
 
       val panned = LinPan2.ar(pure, LinLin("signedPan".kr(0.0), -1.0, 1.0, -0.9, 0.9))
